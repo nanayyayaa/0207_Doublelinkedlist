5ef021ef937485be2f6fc8c8f157c74a4940b923
nanayyayaa <jiejueejuju@gmail.com>
2026-04-28 12:48:13 +0700

    commit8

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -108,5 +108,22 @@ public:
 
         // Step 2: if node is at the beginning
         if (current == START)
+        {
+            START = current->next; // Step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b: START.prev = NULL
+        }
+        else
+        {
+            // Step 3: Link previos node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: If current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 5: Delete the node
+        delete current;
     }
 }
