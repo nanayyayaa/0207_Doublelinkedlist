7641388d736e331dec403d92367baf08a978513a
nanayyayaa <jiejueejuju@gmail.com>
2026-04-28 11:53:13 +0700

    first commit

---
+++ DoubleLinkedList.cpp
@@ -0,0 +1,18 @@
+// 4NimBelakang_DoubleLinkedList
+#include <iostream>
+#include <string>
+using namespace std;
+
+class 4NimBelakang_DoubleLinkedList
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class 4NimBelakang_DoubleLinkedList
+{
+private:
+    Node *START;
+}
