4d4ab66596a3d7eb10f001b417b61e03d32bb8a9
nanayyayaa <jiejueejuju@gmail.com>
2026-04-28 12:35:10 +0700

    commit6

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -74,5 +74,22 @@ public:
         // Step 9: Insert between current and current->next
         newNode->next = current->next; // Step 9a: newNode.next = current.next
         newNode->prev = current        // Step 9b: newNode.prev = current
+
+        // insert Last node
+        if (current->next != NULL)
+            current->next->prev = newNode; // Step 9c: current.next.prev = newNode
+
+        current->next = newNode; // Step 9d: current.next = newNode
+    }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "n\list is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
     }
 }
